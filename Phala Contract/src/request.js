e6237b8caf148d82b4188c66632f281f92a8a95b
Shubham Patel <shubhamashishpatel@gmail.com>
2023-11-24 01:14:34 +0530

    Update request.js

--- Phala Contract/src/request.js
+++ Phala Contract/src/request.js
@@ -7,9 +7,9 @@ const axios = require("axios");
 const ethers = require("ethers")
 app = express(); // Initializing app 
 
-const contract_address ="0x3B04EFf99792d8d299E798Aa15ead9b8bB77b344";
-const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai");
-const your_private_key_string="1b4ef2764fb0b762512aca9558a76db21db34c80b549e782e4ef8018a831dce2";
+const contract_address ="0x17121eB13DFA6B6575e8b4E6b4Eb5b47AE2F5A5F";
+const provider = new ethers.providers.JsonRpcProvider("https://polygon.llamarpc.com");
+const your_private_key_string=`${process.env.PK}`;
 const signer = new ethers.Wallet(your_private_key_string, provider);
 const contractObj = new ethers.Contract(contract_address,abi,signer);
 
